6da73a3f143ba153bdce492555a942a08ca553ea
Ian Yap <ian@royalcourt.com>
2015-10-10 23:12:40 -0700

    More changes to setting up the SetupRunController

--- RunRabbit/SetupRunViewController.h
+++ RunRabbit/SetupRunViewController.h
@@ -8,8 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SetupRunViewController : UIViewController
+@interface SetupRunViewController : UIViewController   <UIPickerViewDataSource,UIPickerViewDelegate>
 
+@property (strong, nonatomic) IBOutlet UIPickerView *distancePicker;
+
+@property (strong, nonatomic) IBOutlet UIPickerView *timePicker;
+
+@property (strong, nonatomic) NSArray *distanceChoices;
+
+@property (strong, nonatomic) NSArray *timeChoices;
 
 @end
 
--- RunRabbit/SetupRunViewController.m
+++ RunRabbit/SetupRunViewController.m
@@ -17,11 +17,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //initialize picker values
+    self.distanceChoices  = [[NSArray alloc]  initWithObjects:@"1/2 mile",@"1 mile",@"2 miles",@"3 miles",@"4 miles",@"5 miles" , nil];
+    self.timeChoices  = [[NSArray alloc]  initWithObjects:@"5 mins",@"10 mins",@"15 mins",@"20 mins",@"25 mins",@"30 mins", nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+/*
+ * Picker stuff
+ */
+
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+    
+}
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent: (NSInteger)component {
+    return 6;
+    
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row   forComponent:(NSInteger)component {
+    
+    return [self.timeChoices objectAtIndex:row];
+    
+}
+
 
 @end
